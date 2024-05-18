6ba17ea52fda81df367b2d4488abcc73df7cda80
Diego Artiles <diego@dartiles.dev>
2024-05-18 10:42:37 -0300

    feat(packages/ui): add new ui components like avatar, pagination, skeleton, input, etc

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,3 +1,7 @@
 export * from './button'
 export * from './card'
 export * from './navigation-menu'
+export * from './input'
+export * from './avatar'
+export * from './skeleton'
+export * from './pagination'
