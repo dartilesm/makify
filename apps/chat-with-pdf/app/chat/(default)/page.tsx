5fcc6a293e0b8eb3cf24e7c4e15a5870e412da74
Diego Artiles <diego@dartiles.dev>
2024-08-02 16:36:36 -0300

    chore(apps/chat-with-pdf): fix build

--- apps/chat-with-pdf/app/chat/(default)/page.tsx
+++ apps/chat-with-pdf/app/chat/(default)/page.tsx
@@ -10,10 +10,6 @@ async function getChats() {
 }
 
 export default async function Page() {
-  console.log("This are the variables defined", {
-    DATABASE_URL: process.env.DATABASE_URL,
-    DIRECT_URL: process.env.DIRECT_URL,
-  });
   const chats = await getCachedChats();
 
   return <ChatsContainer chats={chats} />;
