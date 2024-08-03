fde804015d42a1cb97cb2301f63437100a81384c
Diego Artiles <diego@dartiles.dev>
2024-08-03 11:40:41 -0300

    refactor(apps/chat-with-pdf): change the app name

--- apps/chat-with-pdf/app/components/header.tsx
+++ apps/chat-with-pdf/app/components/header.tsx
@@ -1,6 +1,5 @@
 import { Chat } from "@prisma/client";
 import { DocumentSwitcher } from "./header/document-switcher/document-switcher";
-import { UserNav } from "./header/user-nav";
 
 type HeaderProps = {
   chats: Chat[];
@@ -10,7 +9,12 @@ export async function Header({ chats = [] }: HeaderProps) {
   return (
     <header className="border-b">
       <div className="flex h-20 flex-row items-center justify-evenly px-4">
-        <div className="text-sm">Chat with PDF</div>
+        <div className="col flex flex-col gap-1 text-sm">
+          Chat with PDF
+          <span className="text-muted-foreground text-right text-xs">
+            by Makify ✨
+          </span>
+        </div>
         <div className="flex flex-1 justify-center">
           {!!chats.length && <DocumentSwitcher chats={chats} />}
         </div>
