647f5cca0e2f60d7ff06ca98f3df5ccd0c5c9efc
Diego Artiles <diego@dartiles.dev>
2024-08-03 12:30:00 -0300

    feat(apps/chat-with-pdf): add empty state on chats

--- apps/chat-with-pdf/app/components/pages-containers/chats-container.tsx
+++ apps/chat-with-pdf/app/components/pages-containers/chats-container.tsx
@@ -16,6 +16,7 @@ import { useState } from "react";
 import { NewDocumentDialog } from "../header/new-document-dialog/new-document-dialog";
 import { Container } from "../ui/container";
 import { Heading } from "../ui/heading";
+import { SadFaceIcon } from "icons/sad-face";
 
 type ChatsContainerProps =
   | {
@@ -61,8 +62,8 @@ export function ChatsContainer({
         <Separator />
       </div>
       <Container className="flex-1">
-        <Card className="col-span-4 h-full hover:shadow-none">
-          <CardContent className="flex flex-col gap-4 p-4">
+        <Card className="col-span-4 flex h-full hover:shadow-none">
+          <CardContent className="flex flex-1 flex-col gap-4 p-4">
             {!loading &&
               chats?.map((chat) => (
                 <Card className="flex flex-row items-center justify-between p-4">
@@ -99,7 +100,9 @@ export function ChatsContainer({
                     </div>
                   </div>
                   <Button className="flex-shrink-0" variant="outline" asChild>
-                    <Link href={`/chat/${chat.id}`}>Open chat</Link>
+                    <Link href={`/chat/${chat.id}`}>
+                      Open the chat and let the adventure begin
+                    </Link>
                   </Button>
                 </Card>
               ))}
@@ -107,6 +110,15 @@ export function ChatsContainer({
               fakeChatsList.map((_, index) => (
                 <Skeleton className="h-20 w-full" />
               ))}
+            {!loading && chats?.length === 0 && (
+              <div className="flex h-full flex-col items-center justify-center gap-4">
+                <SadFaceIcon className="h-28 w-28 fill-gray-600 opacity-30" />
+                <p className="text-muted-foreground max-w-72 text-center">
+                  Oh no! No chats found. Start a new chat to get the party
+                  started.
+                </p>
+              </div>
+            )}
           </CardContent>
         </Card>
       </Container>
