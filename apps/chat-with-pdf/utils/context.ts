7eb6e14bb38fac3761d03d7df56a59e27c4c626d
Diego Artiles <diego@dartiles.dev>
2024-08-25 11:07:29 -0300

    feat(apps/chat-with-pdf): get context from supabase

--- apps/chat-with-pdf/utils/context.ts
+++ apps/chat-with-pdf/utils/context.ts
@@ -1,42 +1,22 @@
-import { getPineconeClient } from "./pinecone.client";
+import { supabase } from "./supabase";
 import { getEmbeddings } from "./vector-store";
-
-async function getMatchesFromEmbeddings(
-  embeddings: number[],
-  documentId: string,
-) {
-  const pineconeClient = await getPineconeClient(documentId);
-
-  try {
-    const queryResult = await pineconeClient.query({
-      topK: 5,
-      vector: embeddings,
-      includeMetadata: true,
-    });
-
-    return queryResult.matches || [];
-  } catch (error) {
-    console.log("error querying embeddings", error);
-    throw error;
-  }
-}
 
 export async function getContext(query: string, documentId: string) {
   // User query embeddings
   const userQueryEmbeddings = await getEmbeddings(query);
 
-  // Get matches from Pinecone
-  const matches = await getMatchesFromEmbeddings(
-    userQueryEmbeddings,
-    documentId,
+  const { data: documentSections, error } = await supabase.rpc(
+    "match_documents",
+    {
+      query_embedding: userQueryEmbeddings,
+      match_threshold: 0.7,
+      match_count: 200,
+      document_id: documentId,
+    },
   );
 
-  const qualifiedMatches = matches.filter((match) => match?.score ?? 0 > 0.7);
-
-  const textContent = qualifiedMatches.reduce((acc, match) => {
-    const { metadata } = match;
-    const pageNumber = metadata?.pageNumber;
-    const text = metadata?.text;
+  const supabaseTextContent = documentSections.reduce((acc, match) => {
+    const { pageNumber, textChunk } = match;
     // Return the accumulator in this format:
     // START PAGE 1 BLOCK
     // Text extracted from page 1
@@ -44,22 +24,15 @@ export async function getContext(query: string, documentId: string) {
     if (!acc.includes(`START PAGE ${pageNumber} BLOCK`)) {
       acc += `START PAGE ${pageNumber} BLOCK\n`;
     }
-    acc += `${text}\n`;
+    acc += `${textChunk}\n`;
 
     return acc;
   }, "");
 
-  const docs = qualifiedMatches.map((match) => match.metadata?.text);
-  const pageNumbers = qualifiedMatches
-    .map((match) => match.metadata?.pageNumber)
-    .filter((pageNumber, index, self) => self.indexOf(pageNumber) === index);
-
-  const context = {
-    text: docs.join("\n").substring(0, 3000),
-    pageNumbers,
-  };
+  const textContentNormalized = supabaseTextContent.replace(/\n/g, " ");
+  console.log(textContentNormalized);
 
   // Limit the block text to 3000 characters
 
-  return textContent.substring(0, 3000);
+  return textContentNormalized.substring(0, 3000);
 }
