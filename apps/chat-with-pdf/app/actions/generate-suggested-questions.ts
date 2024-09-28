81c43b87efe66738db32a1e9c1f2da7bfcb8dabd
Diego Artiles <diego@dartiles.dev>
2024-09-28 15:09:14 -0300

    feat(apps/chat-with-pdf): add generate-suggested-question action

---
+++ apps/chat-with-pdf/app/actions/generate-suggested-questions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { getContext } from "@/lib/context";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
+import { z } from "zod";
+
+export async function generateSuggestedQuestions(documentId: string) {
+  const documentSummary = await getContext(
+    "Give me a summary of the document.",
+    documentId,
+  );
+
+  const { object } = await generateObject({
+    model: google("gemini-1.5-flash-latest"),
+    schema: z.object({
+      questions: z.array(z.string()).min(3).max(5),
+    }),
+    prompt: `You are an AI assistant that generates a list of 3-5 suggested questions based on the following document summary. The questions should be diverse and cover different aspects of the document: ${documentSummary}`,
+  });
+
+  return object;
+}
